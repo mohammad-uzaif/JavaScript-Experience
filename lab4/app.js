7b22da5c3c8711efbeac80dddec3bf482073fad3
mohammad-uzaif <mohammaduzaif0111@gmail.com>
2025-12-07 03:29:53 +0530

    lab5

---
+++ lab4/app.js
@@ -0,0 +1,6 @@
+let empty ="";
+console.log(typeof empty,empty.length);
+
+
+let space = " ";
+console.log(typeof space, space.length);
---
+++ lab5/app.js
@@ -0,0 +1,11 @@
+let color = "green";
+
+if(color == "red"){
+    console.log("Stop : Red color is to stop");
+}
+if(color == "yellow"){
+    console.log("Slow : Yellow color is to go slow or ready");
+}
+if(color == "green"){
+    console.log("Go : Green color is to ready to go");
+}
