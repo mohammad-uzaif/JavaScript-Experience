ce86f9a1f9d780fb288c23b1910c9b5b37e074cc
mohammad-uzaif <mohammaduzaif0111@gmail.com>
2025-12-07 10:06:10 +0530

    lab10

---
+++ lab10/app.js
@@ -0,0 +1,5 @@
+let firstName = prompt("Enter User Name :");
+let age = prompt("Enter Your Age :");
+
+// let str = firstName "is" + age "years old";
+alert(firstName + " is " + age +" years old.");
---
+++ lab11/app.js
@@ -0,0 +1,19 @@
+let quater = 4;
+
+switch (quater) {
+    case 1:
+        console.log("January, February, March")
+        break;
+    case 2:
+        console.log("April, May, June");
+        break;
+    case 3:
+        console.log("July, August, September");
+        break;
+    case 4:
+        console.log("October, November, December");
+        break;
+    default:
+        console.log("! only enter range in (1-4) only");
+        break;
+}
